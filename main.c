a350fd8c45d4909d3eaebe80f485c96f3385d342
Samip Poudel <144611884+poudelsamip@users.noreply.github.com>
2024-03-18 22:27:06 +0545

    fixed some minor bugs and optimized the code
    added a boolean function to check if book exists in the library or not and use that value in all other functions.

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ typedef struct lib{
     char borrowersName[100];
     bool isBorrowed;
 }lib;
-
+bool checkBook(lib book[],int* num,char selectedBook[]);
 void addBook(lib book[], int* num);
 void removeBook(lib book[],int* num);
 void viewBook(lib book[], int* num);
@@ -67,36 +67,67 @@ int main(){
     return 0;
 }
 
+bool checkBook(lib book[],int* num,char selectedBook[]){
+    bool bookFound=false;
+    for (int i = 0; i < *num; i++) {
+        if (strcmp(book[i].title,selectedBook)==0) {
+            bookFound = true;
+            break;
+        }
+    }
+    return bookFound;
+}
+
 void addBook(lib book[],int* num){
-  
+    char choice;
+    char bookName[20];
     printf("\n-- ADD BOOK TO LIBRARY-- \n");
-    printf("\nEnter Book Name : ");
-    scanf(" %[^\n]",&book[*num].title);
-   
-    printf("Enter Author Name : ");
-    scanf(" %[^\n]",&book[*num].author);
-    
-    printf("\n!!  %s by %s SUCCESSFULLY ADDED !!\n",book[*num].title,book[*num].author);
-    (*num)++;
+    do {
+        printf("\nEnter Book Name : ");
+        scanf(" %[^\n]",&bookName);
+        
+        if (!checkBook(book, num, bookName)) {
+            strcpy(book[*num].title,bookName);
+            printf("Enter Author Name : ");
+            scanf(" %[^\n]",&book[*num].author);
+        
+            printf("\n!!  %s by %s SUCCESSFULLY ADDED !!\n",book[*num].title,book[*num].author);
+            (*num)++;
+        }
+        else printf("\nBook is already present in library.\n");
+        
+        printf("\nDo you want to add more bookS? (Y/N) : ");
+        scanf(" %c",&choice);
+    } while (choice == 'y' || choice == 'Y');
     return ;
 }
 
 void removeBook(lib book[],int* num){
     printf("\n-- REMOVE BOOK FROM LIBRARY --\n");
     char remove[100];
-    printf("\nEnter a book to remove: ");
-    scanf(" %[^\n]",&remove);
-    for(int i=0;i<*num;i++){
-        if(strcmp(book[i].title,remove)==0){
-            for(int j=i;j<*num;j++){
-                strcpy(book[j].title,book[j+1].title);
-                strcpy(book[j].author,book[j+1].author);
+    char choice;
+    do {
+        printf("\nEnter a book to remove: ");
+        scanf(" %[^\n]",&remove);
+        
+        if(checkBook(book, num, remove)) {
+            for(int i=0;i<*num;i++){
+                if(strcmp(book[i].title,remove)==0){
+                    for(int j=i;j<*num;j++){
+                        strcpy(book[j].title,book[j+1].title);
+                        strcpy(book[j].author,book[j+1].author);
+                    }
+                    (*num)--;
+                    printf("Book Removed successfully :(\n");
+                    break;
+                }
             }
         }
-    }
-    (*num)--;
-    printf("Book Removed successfully :(\n");
-    return ;
+        else printf("\nBook is not in library\n");
+        printf("\nDo you want to remove more books? (Y/N) : ");
+        scanf(" %c",&choice);
+    } while (choice == 'y' || choice == 'Y');
+    return;
 }
 
 void viewBook(lib book[],int* num){
@@ -113,11 +144,18 @@ void searchBook(lib book[],int* num){
     char search[50];
     printf("\nEnter a book to search: ");
     scanf(" %[^\n]",&search);
-    for(int i=0;i<*num;i++){
-        if(strcmp(search,book[i].title)==0){
-            printf("Book Found\n");
-            printf("%s by %s\n",book[i].title,book[i].author);
+    
+    if(checkBook(book, num, search)) {
+        for(int i=0;i<*num;i++){
+            if(strcmp(search,book[i].title)==0){
+                printf("Book Found\n");
+                printf("%s by %s\n",book[i].title,book[i].author);
+                return;
+            }
         }
+    }
+    else{
+        printf("\nBook not found\n");
         return;
     }
 }
@@ -128,21 +166,27 @@ void borrowBook(lib book[],int* num){
     printf("\n-- BORROW BOOK FROM LIBRARY  --\n");
     printf("\nEnter book name: ");
     scanf(" %[^\n]",&borrow);
-    for(int i=0;i<*num;i++){
-        if(strcmp(borrow,book[i].title)==0){
-            if(book[i].isBorrowed == false){
-                printf("Enter Borrower's Name : ");
-                scanf(" %[^\n]",&name);
-                book[i].isBorrowed = true;
-                printf("\n%s borrowed by %s\n",book[i].title,name);
-                strcpy(book[i].borrowersName,name);
-                return;
-            }
-            else{
-                printf("\nBook is already borrowed");
-                return;
+    if(checkBook(book,num,borrow)){
+        for(int i=0;i<*num;i++){
+            if(strcmp(borrow,book[i].title)==0){
+                if(book[i].isBorrowed == false){
+                    printf("Enter Borrower's Name : ");
+                    scanf(" %[^\n]",&name);
+                    book[i].isBorrowed = true;
+                    printf("\n%s borrowed by %s\n",book[i].title,name);
+                    strcpy(book[i].borrowersName,name);
+                    return;
+                }
+                else{
+                    printf("Book is already borrowed by someone else.\n");
+                    return;
+                }
             }
         }
+    }
+    else{
+        printf("\nBook not available in Library\n");
+        return;
     }
 }
 
@@ -151,12 +195,18 @@ void returnBook(lib book[],int* num){
     printf("\n--  RETURN BOOK TO LIBRARY  --\n");
     printf("\nEnter book name: ");
     scanf(" %[^\n]",&borrow);
-    for(int i=0;i<*num;i++){
-        if(strcmp(borrow,book[i].title)==0){
-            book[i].isBorrowed = false;
-            printf("\n%s returned by %s\n", book[i].title,book[i].borrowersName);
-            strcpy(book[i].borrowersName,"");
-            return;
+
+    if(checkBook(book, num, borrow)) {
+        for(int i=0;i<*num;i++){
+            if(strcmp(borrow,book[i].title)==0){
+                book[i].isBorrowed = false;
+                printf("\n%s returned by %s\n", book[i].title,book[i].borrowersName);
+                strcpy(book[i].borrowersName,"");
+                return;
+            }
         }
+    }
+    else{
+        printf("\nThis book was not borrowed\n");
     }
 }
